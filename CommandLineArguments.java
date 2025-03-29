cf9143bd32cf6974e68ad45b4e09fbae52e2802f
vinayreddymallepally <vinayreddy2609@gmail.com>
2025-03-29 12:48:23 +0530

    Add files via upload

---
+++ CommandLineArguments.java
@@ -0,0 +1,13 @@
+package com.journaldev.examples;
+
+public class CommandLineArguments {
+
+	public static void main(String[] args) {
+		System.out.println("Number of Command Line Argument = "+args.length);
+		
+		for(int i = 0; i< args.length; i++) {
+			System.out.println(String.format("Command Line Argument %d is %s", i, args[i]));
+		}
+	}
+
+}
