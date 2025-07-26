7279a203a4c6fc313c0c75aad89fd63abb92e2f3
vinayreddymallepally <vinayreddy2609@gmail.com>
2025-07-26 10:35:18 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student {
+    public static void main(String[] args) {
+        String name = "Sai Vinay";
+        String rollNo = "245122737009";
+        String dept = "Information Technology";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Department: " + dept);
+    }
+}
+
